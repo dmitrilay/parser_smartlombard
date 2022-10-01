a44c0c1713829ca949ee91862659a18fbdf905ec
dmitrilay <dmitrilay@gmail.com>
2022-10-01 19:33:49 +0600

    Новые методы класса

--- main/a_file_system.py
+++ main/a_file_system.py
@@ -7,29 +7,48 @@ BASE_DIR = os.path.join(head, 'data')
 
 
 class WorkFolderFiles():
-    def __init__(self):
+    def __init__(self, search_folder='html_page'):
         """search_folder - будем искать файлы в этой папке"""
-        self.search_folder = 'html_page'
+        self.search_folder = search_folder
         self.list_file = {}
         self.data = None
         self.json_data = None
 
-    def find_file(self):
-        head, tail = os.path.split(__file__)
-        _p = os.path.normpath(f'{head}/data/{self.search_folder}')
-        for file_folder in os.listdir(_p):
-            full_path = os.path.join(_p, file_folder)
+    def find_file(self, search_folder):
+        _path = os.path.join(BASE_DIR, search_folder)
+        for file_folder in os.listdir(_path):
+            full_path = os.path.join(_path, file_folder)
             self.list_file[file_folder] = [full_path, file_folder]
 
     def open_file(self, file_folder):
-        with open(file_folder, 'r', encoding='utf-8') as file:
-            self.data = file.read()
+        _isfile = False
+        _path = os.path.join(BASE_DIR, file_folder)
+        _path = os.path.normpath(_path)
 
-    def save_to_json(self, obj):
-        head, tail = os.path.split(__file__)
-        _p = os.path.normpath(f'{head}/data/json/json.txt')
+        if os.path.isfile(file_folder):
+            _isfile = True
+        elif os.path.isfile(_path):
+            _isfile = True
+            file_folder = _path
+
+        self.data = ''
+        if _isfile:
+            with open(file_folder, 'r', encoding='utf-8') as file:
+                self.data = file.read()
+
+    def save_to_json(self, obj=None, name_file=None):
+        _path = os.path.normpath(f'{BASE_DIR}/{name_file}')
         self.json_data = json.dumps(obj)
-        self.write_file(_p, self.json_data)
+        self.write_file(_path, self.json_data)
+
+    def open_to_json(self, name_file=None):
+        _path = os.path.normpath(f'{BASE_DIR}/{name_file}')
+        self.open_file(_path)
+        if self.data:
+            self.json_data = json.loads(self.data)
+            return self.json_data
+        else:
+            return None
 
     @staticmethod
     def write_file(name_file, obj):
