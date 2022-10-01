d9be1b74347b8ce40bb030bab54c17077068e580
dmitrilay <dmitrilay@gmail.com>
2022-10-01 19:30:42 +0600

    Отправка данных на сервер

--- main/a_data_requests.py
+++ main/a_data_requests.py
@@ -1,18 +1,30 @@
 import json
 import requests
+from requests.auth import AuthBase
 
 
-def sending_data():
-    url = 'http://127.0.0.1:8000/spec/add-characteristic-ajax/'
+class TokenAuth(AuthBase):
+    def __init__(self, token):
+        self.token = token
 
-    # requests.post('https://ch-shop.ru/', data={'key': 'value'})
-    # requests.post(url, data={'key': 'value'})
-    _r = requests.get(url)
-    # print(_r.status_code)
-    # print(_r.content)
-    _d = json.loads(_r.text)
-    # print(type())
-    return (_d)
+    def __call__(self, r):
+        r.headers['X-TokenAuth'] = f'{self.token}'
+        return r
 
 
-# sending_data()
+token = "fjwoapfjow@204diojwa!24dkapwojfjjf22401jdwa190jd(odwa"
+
+
+def sending_spec(obj=0):
+    pathname = 'smartlombard/v2/'
+    host = 'http://127.0.0.1:8000/'
+    url = f'{host}{pathname}'
+
+    _json = json.dumps(obj)
+    # url = 'http://127.0.0.1:8000/spec/add-characteristic-ajax/'
+    _r = requests.post(url, auth=TokenAuth(token), data={'data': _json})
+
+    # print(obj)
+    for key, value in obj.items():
+        if key == '467':
+            print(key, value)
