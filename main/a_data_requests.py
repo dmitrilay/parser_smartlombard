5ba6314b8f700034a60f08aaf84b0eec847e43f7
dmitrilay <dmitrilay@gmail.com>
2022-10-02 17:10:37 +0600

    Создание dev и local файлов

--- main/a_data_requests.py
+++ main/a_data_requests.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from requests.auth import AuthBase
+from settings import *
 
 
 class TokenAuth(AuthBase):
@@ -12,21 +13,16 @@ class TokenAuth(AuthBase):
         return r
 
 
-token = "fjwoapfjow@204diojwa!24dkapwojfjjf22401jdwa190jd(odwa"
-
-
 def sending_spec(obj=0):
     if obj:
-        pathname = 'smartlombard/v2/'
-        host = 'http://127.0.0.1:8000/'
+        pathname = PATH_NAME
+        host = HOST
         url = f'{host}{pathname}'
 
         _json = json.dumps(obj)
-        # url = 'http://127.0.0.1:8000/spec/add-characteristic-ajax/'
         _r = requests.post(url, auth=TokenAuth(token), data={'data': _json})
 
-        # print(obj)
-        for key, value in obj.items():
-            print(key, value)
+        # for key, value in obj.items():
+        #     print(key, value)
     else:
         print('Нет данных для отправки!')
--- main/settings.py
+++ main/settings.py
@@ -1,10 +1,4 @@
-import os
-import datetime
-
-"""Авторизация"""
-url = 'https://online.smartlombard.ru/login/'
-username = "admin@ch-shop.ru"
-password = "58800111"
+DEBUG = False
 
 """Настройки браузера"""
 options_web = {
@@ -26,3 +20,24 @@ FOLDERS = [
     'month/repurchase',
     'month/sale',
 ]
+
+try:
+    if DEBUG:
+        from settings_local import *
+    else:
+        from settings_dev import *
+
+except:
+    print('settings_dev', 'settings_local', "файлы не найдены")
+
+    """Авторизация"""
+    url = 'https://online.smartlombard.ru/login/'
+    username = ""
+    password = ""
+
+    """Авторизация на ch-shop.ru"""
+    token = "111111111"
+
+    """Данные для сайта"""
+    PATH_NAME = 'smartlombard/v2/v2/v3'
+    HOST = 'http://127.0.0.1:8000/'
